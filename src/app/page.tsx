e266d42f8598ea2a617a6bb992c16aab3e645dc5
monichre <liamhellis@gmail.com>
2025-06-09 09:12:04 -0500

    minor cosmetic css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-
-
 import {Home} from '@/components/home/home-alt'
 import {DecorativeElements} from '@/components/home/decorative-elements'
 
 export default async function Index() {
-  
   // bg-[#F5F2ED]
   return (
-    <div className='h-auto relative overflow-hidden flex flex-col align-middle justify-start items-center'>
-      
+    <div className='h-full relative  flex flex-col align-middle justify-start items-center'>
       <DecorativeElements />
 
       <Home />
